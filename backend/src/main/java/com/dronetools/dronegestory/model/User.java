650205940acecab58a46eed230cda9a334392d18
kfuentesb <miraclehawk3@gmail.com>
2026-03-04 11:39:29 +0100

    Add extra dependencies and first entitie User

---
+++ backend/src/main/java/com/dronetools/dronegestory/model/User.java
@@ -0,0 +1,40 @@
+package com.dronetools.dronegestory.gestordrones.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_ID")
+    private Integer userID;
+
+    @Column(name = "operator_id")
+    private Integer operatorID;
+
+    @Column(name = "name", nullable = false, length = 100)
+    private String name;
+
+    @Column(name = "last_names", nullable = false, length = 150)
+    private String lastNames;
+
+    @Column(name = "username", nullable = false, unique = true, length = 80)
+    private String username;
+
+    @Column(name = "password", nullable = false, length = 255)
+    private String password;
+
+    @Column(name = "email", nullable = false, unique = true, length = 120)
+    private String email;
+
+    @Column(name = "phone", length = 20)
+    private String phone;
+
+    @Column(name = "path_imagen", length = 255)
+    private String pathImagen;
+
+}
