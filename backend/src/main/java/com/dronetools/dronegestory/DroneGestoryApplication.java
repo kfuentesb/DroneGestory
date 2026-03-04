6082f57c34d232b4527202b601af63c071136eea
kfuentesb <miraclehawk3@gmail.com>
2026-03-04 09:22:13 +0100

    First commit

---
+++ backend/src/main/java/com/dronetools/dronegestory/DroneGestoryApplication.java
@@ -0,0 +1,13 @@
+package com.dronetools.dronegestory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroneGestoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DroneGestoryApplication.class, args);
+    }
+
+}
