73dc3ad502eb68a13a7bd4dbf2f23aec55ecc862
AncheJeez <andresjsangon@gmail.com>
2026-03-23 09:06:38 +0100

    reestructurada las entidad anexo

--- backend/src/main/java/com/dronetools/dronegestory/model/anexos/Anexo8.java
+++ backend/src/main/java/com/dronetools/dronegestory/model/anexos/Anexo8.java
@@ -1,50 +1,25 @@
 package com.dronetools.dronegestory.model.anexos;
 
-import com.dronetools.dronegestory.common.AnexoVersionado;
 import com.dronetools.dronegestory.model.Operation;
-import com.dronetools.dronegestory.model.enums.AnexoStatus;
 import jakarta.persistence.*;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
+// import lombok.NoArgsConstructor;
 import lombok.Setter;
+import com.dronetools.dronegestory.model.Anexo;
 
 @Entity
 @Table(name = "anexo8")
-@Getter @Setter @NoArgsConstructor
-public class Anexo8 implements AnexoVersionado {
-    @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
-    @Column(name = "id_anexo8")
-    private Long id;
+@Getter
+@Setter
+// @NoArgsConstructor
+public class Anexo8 extends Anexo {
+    
+    @Column(name = "texto_prueba")
+    private String textoPrueba;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "id_operacion", nullable = false)
-    private Operation operation;
-
-    @Column(name = "campo_anexo8")
-    private String campoAnexo8;
-
-    // Versión
-    @Column(name = "version_number", nullable = false)
-    private int numeroVersion;
-
-    // Estado
-    @Enumerated(EnumType.STRING)
-    @Column(name = "estado", length = 20, nullable = false)
-    private AnexoStatus estado = AnexoStatus.BORRADOR;
-
-//    @Column(name = "fecha_hora_prevista")
-//    private LocalDateTime fechaHoraPrevista;
-//
-//    @Column(name = "medios_materiales")
-//    private String mediosMateriales;
-//
-//    @Column(name = "direccion")
-//    private String direccion;
-//
-//    @Column(name = "coords")
-//    private String coords;
-//
-//    @Column(name = "imagen_espacio_aereo")
-//    private String imagenEspacioAereo;
+    public Anexo8() {
+        super();
+        this.setTipoAnexo(7);
+        // this.setEstado(AnexoStatus.BORRADOR);
+    }
 }
