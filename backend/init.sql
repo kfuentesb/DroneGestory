c6a476bf500c4f187c4f9d402791d78215da0ddb
AncheJeez <andresjsangon@gmail.com>
2026-03-24 16:35:56 +0100

    cambiado de cmartinez a admin

--- backend/init.sql
+++ backend/init.sql
@@ -1,11 +1,11 @@
 INSERT INTO app_user
-(type, first_name, last_name, username, password_hash, email, phone_number, image_path)
+(type, first_name, last_name, username, password_hash, email, phone_number)
 VALUES
-('ADMIN', 'Carlos', 'Martinez Lopez', 'cmartinez', '$2b$12$6cdleoSsH9EtO.vo2bT84Ohltj4SthGTl5dZbsV0bDXogVEEXuICS', 'carlos@skydron.es', 600111333, 'human-1.jpg'),
-('MANAGER', 'Lucia', 'Fernandez Ruiz', 'lfernandez', '$2b$12$6cdleoSsH9EtO.vo2bT84Ohltj4SthGTl5dZbsV0bDXogVEEXuICS', 'lucia@skydron.es', 600111444, 'human-2.jpg'),
-('PILOT', 'Miguel', 'Santos Perez', 'msantos', '$2b$12$6cdleoSsH9EtO.vo2bT84Ohltj4SthGTl5dZbsV0bDXogVEEXuICS', 'miguel@aerovision.es', 600333555, 'human-3.jpg'),
-('PILOT', 'Dani', 'Marquez Romero', 'dmarquez', '$2b$12$6cdleoSsH9EtO.vo2bT84Ohltj4SthGTl5dZbsV0bDXogVEEXuICS', 'dani@aerovision.es', 600444666, 'human-4.jpg'),
-('PILOT', 'Juan', 'Sánchez Blanco', 'jsanchez', '$2b$12$6cdleoSsH9EtO.vo2bT84Ohltj4SthGTl5dZbsV0bDXogVEEXuICS', 'juan@aerovision.es', 600555777, 'human-5.jpg');
+('ADMIN', 'ADMIN', 'ADMIN', 'admin', '$2b$12$6cdleoSsH9EtO.vo2bT84Ohltj4SthGTl5dZbsV0bDXogVEEXuICS', 'admin@admin.es', 600111333),
+('MANAGER', 'Lucia', 'Fernandez Ruiz', 'lfernandez', '$2b$12$6cdleoSsH9EtO.vo2bT84Ohltj4SthGTl5dZbsV0bDXogVEEXuICS', 'lucia@skydron.es', 600111444),
+('PILOT', 'Miguel', 'Santos Perez', 'msantos', '$2b$12$6cdleoSsH9EtO.vo2bT84Ohltj4SthGTl5dZbsV0bDXogVEEXuICS', 'miguel@aerovision.es', 600333555),
+('PILOT', 'Dani', 'Marquez Romero', 'dmarquez', '$2b$12$6cdleoSsH9EtO.vo2bT84Ohltj4SthGTl5dZbsV0bDXogVEEXuICS', 'dani@aerovision.es', 600444666),
+('PILOT', 'Juan', 'Sánchez Blanco', 'jsanchez', '$2b$12$6cdleoSsH9EtO.vo2bT84Ohltj4SthGTl5dZbsV0bDXogVEEXuICS', 'juan@aerovision.es', 600555777);
 
 
 -- La contraseña está hasheada de "pass123"--
