f5fb9fd670e544223959be3a9f5efcf2c9448561
kfuentesb <miraclehawk3@gmail.com>
2026-03-24 13:07:46 +0100

    Anexo4 completo

--- backend/src/main/java/com/dronetools/dronegestory/model/anexos/Anexo4.java
+++ backend/src/main/java/com/dronetools/dronegestory/model/anexos/Anexo4.java
@@ -15,8 +15,19 @@ import java.time.LocalDateTime;
 // @NoArgsConstructor
 public class Anexo4 extends Anexo {
 
+    // FORMULARIO
+    @Column(name = "codigo")
+    private String codigo;
+
+    @Column(name = "descripcion")
+    private String descripcion;
+
     @Column(name = "fecha_hora_prevista")
     private LocalDateTime fechaHoraPrevista;
+
+    // TODO
+    //private List<String> personal = new ArrayList();
+    //private List<String> uas = new ArrayList();
 
     @Column(name = "medios_materiales")
     private String mediosMateriales;
@@ -27,8 +38,43 @@ public class Anexo4 extends Anexo {
     @Column(name = "coords")
     private String coords;
 
+    // IMÁGENES
     @Column(name = "imagen_espacio_aereo")
     private String imagenEspacioAereo;
+
+    @Column(name = "imagen_zona_vuelo")
+    private String imagenZonaVuelo;
+
+    // BOOLEANS
+    // --- Sección 4. ZONAS GEOGRÁFICAS de UAS ---
+
+    private Boolean espacioAereoControlado;
+    private Boolean estudioAeronauticoCoordinado;
+    private Boolean entornoAerodromos;
+    private Boolean distanciaMinimaInfraestructuras;
+    private Boolean zonasProhibidasFlexible;
+    private Boolean cumpleCondiciones;
+    private Boolean zonasSeguridad;
+    private Boolean permisoPrevioSeguridad;
+    private Boolean serviciosEsencialesComunidad;
+    private Boolean permisoPrevioServicios;
+    private Boolean entornosUrbanos;
+    private Boolean cumplenDistanciasEdificios;
+    private Boolean comunicacionMinisterioInterior;
+    private Boolean zonaResVueloFotografico;
+    private Boolean permisoCecaf;
+    private Boolean zonasProtMedioambiental;
+    private Boolean disponeCoordGestor;
+
+    // --- Seccion 6. Requisitos y limitaciones en zona de vuelo
+    private Boolean conopsYModeloSemantico;
+    private Boolean aplicaModelo;
+    private Boolean defineGeografiaVueloConops;
+    private Boolean defineVolContigencia;
+    private Boolean defineMargenRiesgoTierra;
+    private Boolean defineZonaTerrestreControlada;
+
+
 
     // Así le decimos automáticamente de que se trata del Anexo4 al anexo base
     public Anexo4() {
@@ -37,6 +83,6 @@ public class Anexo4 extends Anexo {
         this.setEstado(AnexoStatus.BORRADOR);
     }
 
-    // INFORMACION SOBRE LAS OPERACIONES
+
 
 }
