2efb6fe460b16e053b38c9d7c0c90d5535e07845
AncheJeez <andresjsangon@gmail.com>
2026-03-26 11:56:31 +0100

    modificado ultimo aircraft

--- backend/init.sql
+++ backend/init.sql
@@ -14,4 +14,4 @@ INSERT INTO aircraft (
 ) VALUES
 (1001, 'DroneCorp', 'Falcon One', 987654, 'C2', 7.120, 1.780, 120.500, 'Multirrotor', 410.325, true),
 (1002, 'AeroMaker Industries', 'Glider X', 764321, 'No', 2.050, 2.950, 60.300, 'Avion', 85.000, false),
-(1003, 'DroneTools', 'Condor', 584924, 'A4', 3.453, 3.650, 75.600, 'Dron', 100.000, true);
+(1003, 'DroneTools', 'Condor', 584924, 'C5', 3.453, 3.650, 75.600, 'Dron', 100.000, true);
