10da566958c3b293d5b165378399cbdda4c00304
kfuentesb <miraclehawk3@gmail.com>
2026-04-07 09:10:02 +0200

    Infobadge reorganizado

--- frontend/src/components/forms/FormAircraft.tsx
+++ frontend/src/components/forms/FormAircraft.tsx
@@ -4,7 +4,7 @@ import Select from 'react-select';
 import { useNavigate } from "react-router-dom";
 import { apiFetch } from '../../api';
 import { aircraftClasses, configs, LIMITS } from '../../global-const/aircraft-const';
-import { InfoBadge } from '../users/UserCertificatesSection';
+import { InfoBadge } from '../commons/InfoBadge';
 import './generic-form.css';
 
 type SelectOption = { value: string; label: string };
