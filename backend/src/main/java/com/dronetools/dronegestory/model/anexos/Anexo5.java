4db33fe677e8a8cd59f6f86317e3b39b3be58e9f
kfuentesb <miraclehawk3@gmail.com>
2026-04-13 18:19:55 +0200

    Agregando los anexos

--- backend/src/main/java/com/dronetools/dronegestory/model/anexos/Anexo5.java
+++ backend/src/main/java/com/dronetools/dronegestory/model/anexos/Anexo5.java
@@ -6,14 +6,59 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @Entity
 @Table(name = "anexo5")
 @Getter
 @Setter
 public class Anexo5 extends Anexo {
 
-    @Column(name = "texto_prueba")
-    private String textoPrueba;
+    @Column(name = "nombre_conops")
+    private String conops;
+
+    @Column(name = "fechaOp")
+    private LocalDateTime fechaOp;
+
+    // BOOLEANS
+    // Seccion 1. Lugar de la operación
+    // 1.1 Evaluación del área
+    // 1.1.1 Terreno, obstáculos y obstrucciones
+    private Boolean vlos;
+    private Boolean ubicacionObservadores;
+    private Boolean evaluacionVisibilidadYAlcance;
+    // 1.1.2 Si la operación se lleva a cabo próxima a aeropuertos, aeródromos y helipuertos
+    private Boolean condicionantesAcordadosConGestor;
+    // 1.1.3 Otros
+    private Boolean analisisEnFuncionConops;
+    private Boolean evaluacionEntornoAereoAdyacente;
+    private Boolean vueloTerrestreControlado;
+    // 1.2
+    // 1.2.1 NOTAM
+    private Boolean notamActivos;
+    private Boolean tsaPreviaNotam;
+    private Boolean procedimientosATSP;
+    // 2 Condiciones ambientales
+    // 2.1
+    private Boolean condicionesClimatologicas;
+    // 3. Personal
+    // 3.1
+    private Boolean personalSabeFunciones;
+    // 4. Procedimientos de comunicación
+    private Boolean comunicacionEntrePersonal;
+    private Boolean comunicacion3Partes;
+    // 5. Requisitos adicionales
+    private Boolean requisitosSeguridad;
+    private Boolean requisitosMedioAmbiente;
+    private Boolean requisitosRadioelectrico;
+    private Boolean requisitosLocalesEspecificos;
+    // 6. Atenuaciones al riesgo
+    private Boolean atenuacionesGRC;
+    private Boolean atenuacionesARC;
+    // 7. El uas esta en condiciones adecuadas para operar
+    private Boolean comprobacionesUasVuelo;
+    // 8. Aptitud para operar TODO
+
 
     public Anexo5() {
         super();
--- backend/src/main/java/com/dronetools/dronegestory/model/anexos/Anexo7.java
+++ backend/src/main/java/com/dronetools/dronegestory/model/anexos/Anexo7.java
@@ -6,15 +6,78 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @Entity
 @Table(name = "anexo7")
 @Getter
 @Setter
 public class Anexo7 extends Anexo {
     
-    @Column(name = "texto_prueba")
-    private String textoPrueba;
+    @Column(name = "nombre_conops")
+    private String nombreConops;
 
+    @Column(name = "fechaOp")
+    private LocalDateTime fechaOp;
+
+    // TODO asignar aeronave
+
+    // 1. Verificación - 13 elementos
+    private Boolean estructuraCorrecto;
+    private String estructuraObservaciones;
+
+    private Boolean bateriasCorrecto;
+    private String bateriasObservaciones;
+
+    private Boolean sensoresCorrecto;
+    private String sensoresObservaciones;
+
+    private Boolean motoresCorrecto;
+    private String motoresObservaciones;
+
+    private Boolean helicesCorrecto;
+    private String helicesObservaciones;
+
+    private Boolean partesMovilesCorrecto;
+    private String partesMovilesObservaciones;
+
+    private Boolean comunicacionesCorrecto;
+    private String comunicacionesObservaciones;
+
+    private Boolean plantaPotenciaCorrecto;
+    private String plantaPotenciaObservaciones;
+
+    private Boolean cargaPagoCorrecto;
+    private String cargaPagoObservaciones;
+
+    private Boolean identificacionRemotaCorrecto;
+    private String identificacionRemotaObservaciones;
+
+    private Boolean sistemaGeoconscienciaCorrecto;
+    private String sistemaGeoconscienciaObservaciones;
+
+    private Boolean datosVueloCorrecto;
+    private String datosVueloObservaciones;
+
+    private Boolean otrosVerificacionCorrecto;
+    private String otrosVerificacionObservaciones;
+
+    // 2. Recogida y almacenaje - 5 elementos
+    private Boolean aeronaveCorrecto;
+    private String aeronaveObservaciones;
+
+    private Boolean unidadControlCorrecto;
+    private String unidadControlObservaciones;
+
+    private Boolean sensoresRecogidaCorrecto;
+    private String sensoresRecogidaObservaciones;
+
+    private Boolean antenasCorrecto;
+    private String antenasObservaciones;
+
+    private Boolean otrosRecogidaCorrecto;
+    private String otrosRecogidaObservaciones;
+    
     public Anexo7() {
         super();
         this.setTipoAnexo(7);
