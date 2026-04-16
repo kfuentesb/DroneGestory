8f4233f3a2e2e7cd3a818cfd2f8f6f4dea1e42ec
AncheJeez <andresjsangon@gmail.com>
2026-04-16 13:45:53 +0200

    ahora en perfil si no hay imagen se muestra default-user, no default-drone

--- frontend/src/components/details/user&profile/ProfileDetail.tsx
+++ frontend/src/components/details/user&profile/ProfileDetail.tsx
@@ -77,6 +77,7 @@ export default function ProfileDetail() {
             onBack={() => navigate("/home")}
             validateForm={validateForm}
             certificateSectionType="user"
+            defaultImage="user"
         />
     );
 }
