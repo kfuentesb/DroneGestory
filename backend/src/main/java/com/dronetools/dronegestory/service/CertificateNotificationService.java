3494e1f26f5ddcb94335bf03fa69fe9cf349fd1d
AncheJeez <andresjsangon@gmail.com>
2026-04-30 12:18:13 +0200

    12:20

--- backend/src/main/java/com/dronetools/dronegestory/service/CertificateNotificationService.java
+++ backend/src/main/java/com/dronetools/dronegestory/service/CertificateNotificationService.java
@@ -21,7 +21,7 @@ public class CertificateNotificationService {
     private JavaMailSender mailSender;
 
     // Se ejecuta todos los días a las 9:00 AM
-    @Scheduled(cron = "0 15 12 * * ?")
+    @Scheduled(cron = "0 20 12 * * ?")
     public void sendExpirationNotifications() {
         // Calculo de la fecha objetivo: hoy + 30 días
         LocalDate today = LocalDate.now();
